48f3aca46566387fbe11af1dbb923918b7414906
kitta-kazushi <kitta-kazushi@users.noreply.github.com>
2017-12-01 22:51:11 +0900

    init

---
+++ src/main/scala/com/example/MyService.scala
@@ -0,0 +1,43 @@
+package com.example
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+import kamon.Kamon
+
+// we don't implement our route structure directly in the service actor because
+// we want to be able to test it independently, without having to spin up an actor
+class MyServiceActor extends Actor with MyService {
+  // the HttpService trait defines only one abstract member, which
+  // connects the services environment to the enclosing actor or test
+  def actorRefFactory = context
+
+  // this actor only runs our route, but you could add
+  // other things here, like request stream processing
+  // or timeout handling
+  def receive = runRoute(myRoute)
+}
+
+
+// this trait defines our service behavior independently from the service actor
+trait MyService extends HttpService {
+  val counter = Kamon.metrics.counter("xxx-counter")
+
+  def xxx(): String = {
+    counter.increment()
+
+    "XXXXXXX"
+  }
+
+  val myRoute =
+    path("") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            "Say hello " + xxx
+          }
+        }
+      }
+    }
+}
